6d9062151d7fb075a00676ce66528576119b485d
Setu Gupta <setu.gupta.2020@gmail.com>
2023-02-25 15:06:26 +0530

    Added code for utilities and modulation
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   README
    	new file:   modems.py
    	new file:   utils.py

---
+++ Q1/modems.py
@@ -0,0 +1,89 @@
+from utils import *
+from math import log2
+import numpy as np
+
+class Modem:
+    def __init__(self, num_bits, symbols):
+       
+        self.num_bits = num_bits
+        self.symbols = symbols
+
+        binary_strings = get_binary_strings(self.num_bits)
+        
+        self.mapping = {}
+        self.reverse_mapping = {}
+        for string, symbol in zip(binary_strings, self.symbols):
+            self.mapping[string] = symbol
+            self.reverse_mapping[symbol] = string
+
+    def modulate(self, bitstream):
+        
+        if len(bitstream) % self.num_bits:
+            prefix_size = self.num_bits - len(bitstream)
+            bitstream = [0] * prefix_size + bitstream
+        
+        modulated_signal = []
+        num_symbols = int(len(bitstream) / self.num_bits)
+        for i in range(num_symbols):
+            bits = bitstream[i : i+self.num_bits]
+            bits_string = "".join([str(x) for x in bits])
+            modulated_signal.append(self.mapping[bits_string])
+
+        return np.asarray(modulated_signal)
+    
+    def demodulate(self, symbols):
+        
+        estimated_symbols = get_estimated_symbols(symbols, self.symbols)
+
+        bit_strings = []
+        for symbol in estimated_symbols:
+            bit_strings.append(self.reverse_mapping[symbol])
+
+        bits = []
+        for string in bit_strings:
+            for bit in string:
+                bits.append(int(bit))
+
+        return bits
+
+class BPSK(Modem):
+
+    def __init__(self):
+        num_bits = 1
+        symbols = get_PSK_points(2)
+        super().__init__(num_bits, symbols)
+
+class QPSK(Modem):
+
+    def __init__(self):
+        num_bits = 2
+        symbols = get_PSK_points(4)
+        super().__init__(num_bits, symbols)
+
+class QAM_4(Modem):
+
+    def __init__(self):
+        num_bits = 2
+        symbols = get_QAM_points(4)
+        super().__init__(num_bits, symbols)
+
+class PSK_16(Modem):
+
+    def __init__(self):
+        num_bits = 4
+        symbols = get_PSK_points(16)
+        super().__init__(num_bits, symbols)
+
+class QAM_16(Modem):
+
+    def __init__(self):
+        num_bits = 4
+        symbols = get_QAM_points(16)
+        super().__init__(num_bits, symbols)
+
+class QAM_32(Modem):
+
+    def __init__(self):
+        num_bits = 5
+        symbols = get_QAM_points(32)
+        super().__init__(num_bits, symbols)
---
+++ Q1/utils.py
@@ -0,0 +1,71 @@
+from math import sqrt, pi, cos, sin
+import numpy as np
+from random import randint
+
+def get_QAM_points(M):
+    points = []
+    coords = int(sqrt(M))
+    shift =  (coords - 1) / 2 
+    
+    for x in range(coords):
+        for y in range(coords):
+            points.append(complex(x - shift, y - shift))
+
+    return points
+
+def get_PSK_points(M):
+    points = []
+    change = (2*pi)/M
+    
+    for x in range(M):
+        arg = x * change
+        real = cos(arg)
+        imag = sin(arg)
+        points.append(complex(real, imag))
+
+    return points
+
+def get_binary_strings(num_bits):
+
+    strings = []
+    for val in range(2**num_bits):
+        bin_string = bin(val)[2:]
+        prefix_length = num_bits - len(bin_string)
+        bin_string = '0' * prefix_length + bin_string
+        strings.append(bin_string)
+
+    return strings
+
+def get_estimated_symbols(recv_symbols, symbols_set):
+    
+    estimated_symbols = []
+
+    for rs in recv_symbols:
+        min_dist = np.Inf
+        estimate = symbols_set[0]
+        for s in symbols_set:
+            if(np.abs(rs - s) < min_dist):
+                min_dist = np.abs(rs - s)
+                estimate = rs
+
+        estimated_symbols.append(estimate)
+
+def awgn(symbols, variance):
+    std_dev = sqrt(variance)
+    n = len(symbols)
+    noise = np.random.normal(loc=0, scale=std_dev, size=(n, 2)).view(np.complex128)
+    noisy_signal = symbols + noise
+    return noisy_signal
+
+def get_random_bits(length):
+    bits = [randint(0, 1) for x in range(length)]
+    return bits
+
+def get_BER(Tx_bits, Rx_bits):
+    total_bits = 0
+    mismatches = 0
+    for tx, rx in zip(Tx_bits, Rx_bits):
+        total_bits += 1
+        mismatches += (tx != rx)
+
+    return (mismatches/total_bits)
