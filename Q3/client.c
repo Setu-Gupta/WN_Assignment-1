3f95f5cfa5dc9ab275f9a00e763fd5e88617935a
Setu Gupta <setu.gupta.2020@gmail.com>
2023-02-26 22:53:32 +0530

    Added socket communication code
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Q3/README
    	modified:   Q3/client.c
    	modified:   Q3/server.c

--- Q3/client.c
+++ Q3/client.c
@@ -2,31 +2,72 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 void print_usage()
 {
-        printf("Incorrect usage. Exiting!\n");
+        fprintf(stderr, "Incorrect usage. Exiting!\n");
         printf("Usage: client <IP> <port>\n");
         exit(-1);
 }
 
 int main(int argc, char* argv[])
 {
+        // Sanitize the user arguments
         if(argc < 3)
                 print_usage();
-
+        
         printf("Client starting...\n");
         printf("Contacting server at %s at port number %s\n", argv[1], argv[2]);
 
-        struct timeval cur_time;
-        if(gettimeofday(&cur_time, NULL))
+        // Create the client socket
+        int sock_fd;
+        if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         {
-                printf("Failed to get current time! Exiting...\n");
+                fprintf(stderr, "Failed to create socket! Exiting...\n");
                 exit(-1);
         }
 
-        unsigned long long time_usec = ((unsigned long long)cur_time.tv_sec * 1000000) + (unsigned long long)cur_time.tv_usec; 
-        printf("Current time in micro_seconds: %lld\n", time_usec);
+        // Create the structure to describe the server
+        struct sockaddr_in server;
+        server.sin_family = AF_INET;
+        server.sin_port = htons((unsigned short) strtoul(argv[2], NULL, 10));
+        if(inet_pton(AF_INET, argv[1], &server.sin_addr) <= 0)
+        {
+                fprintf(stderr, "Could not parse the IP address! Exiting...\n");
+                exit(-1);
+        }
+        
+        // Establish connection with the server
+        if(connect(sock_fd, (struct sockaddr*)&server, sizeof(server)) < 0)
+        {
+                fprintf(stderr, "Could not connect to the server! Exiting...\n");
+                exit(-1);
+        }
 
+        // Send the time stamp 100 times
+        for(int i = 0; i < 100; i++)
+        {
+                // Get the current clock time
+                struct timeval cur_time;
+                if(gettimeofday(&cur_time, NULL))
+                {
+                        fprintf(stderr, "Failed to get current time! Exiting...\n");
+                        exit(-1);
+                }
+                unsigned long long time_usec = ((unsigned long long)cur_time.tv_sec * 1000000) + (unsigned long long)cur_time.tv_usec; 
+                
+                if(send(sock_fd,  &time_usec, sizeof(time_usec), 0) < 0)
+                {
+                        fprintf(stderr, "Failed to send the time stamp! Exiting...\n");
+                        exit(-1);
+                }
+        }
+
+        // Close the file descriptor
+        close(sock_fd);
+
+        printf("Exiting...\n");
         return 0;
 }
--- Q3/server.c
+++ Q3/server.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
 void print_usage()
 {
@@ -12,20 +14,82 @@ void print_usage()
 
 int main(int argc, char* argv[])
 {
+        // Sanitize the user arguments
         if(argc < 2)
                 print_usage();
 
         printf("Server starting at port %s...\n", argv[1]);
         
-        struct timeval cur_time;
-        if(gettimeofday(&cur_time, NULL))
+        // Create the server socket
+        int sock_fd;
+        if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         {
-                printf("Failed to get current time! Exiting...\n");
+                fprintf(stderr, "Failed to create socket! Exiting...\n");
                 exit(-1);
         }
 
-        unsigned long long time_usec = ((unsigned long long)cur_time.tv_sec * 1000000) + (unsigned long long)cur_time.tv_usec; 
-        printf("Current time in micro_seconds: %lld\n", time_usec);
+        // Create the structure to describe the server
+        struct sockaddr_in server;
+        server.sin_family = AF_INET;
+        server.sin_port = htons((unsigned short) strtoul(argv[1], NULL, 10));
+        server.sin_addr.s_addr = INADDR_ANY;
+        
+        // Bind the socket to the specified port
+        if(bind(sock_fd, (struct sockaddr*)&server, sizeof(server)) < 0)
+        {
+                fprintf(stderr, "Failed to bind socket! Exiting...\n");
+                exit(-1);
+        }
 
+        // Start listening for connections
+        if(listen(sock_fd, 0) < 0)
+        {
+                fprintf(stderr, "Failed to start listening! Exiting...\n");
+                exit(-1);
+        }
+        
+        // Accept the connection from the client
+        int client_fd;
+        struct sockaddr_in client;
+        unsigned int client_size = sizeof(client);
+        if((client_fd = accept(sock_fd, (struct sockaddr*)&client, &client_size)) < 0)
+        {
+                fprintf(stderr, "Failed to accept connection! Exiting...\n");
+                exit(-1);
+        }
+
+        // Accept time stamp 100 times
+        for(int i = 0; i < 100; i++)
+        {
+                // Read the client's time stamp
+                unsigned long long client_timestamp;
+                if(read(client_fd, &client_timestamp, sizeof(client_timestamp)) < 0)
+                {
+                        fprintf(stderr, "Failed to read data! Exiting...\n");
+                        exit(-1);
+                }
+                
+                // Get local time stamp
+                struct timeval cur_time;
+                if(gettimeofday(&cur_time, NULL))
+                {
+                        printf("Failed to get current time! Exiting...\n");
+                        exit(-1);
+                }
+                unsigned long long time_usec = ((unsigned long long)cur_time.tv_sec * 1000000) + (unsigned long long)cur_time.tv_usec; 
+                
+                // Compute the latency
+                unsigned long long latency_usec = time_usec - client_timestamp;
+                
+                printf("Latency: %lld\n", latency_usec);
+        }
+
+        // Close the client connection
+        close(client_fd);
+
+        // Shut the server down
+        shutdown(sock_fd, SHUT_RDWR);
+        
+        printf("Exiting...\n");
         return 0;
 }
